9acbeea3f93ba894a3136a612bd9e1fdc80652b9
Andrzej Olender <andrzej.olender@esky.com>
2023-01-21 18:57:18 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# terraform-hetzner-k3s
---
+++ main.tf
@@ -0,0 +1,35 @@
+variable "hetzner_token" {
+
+}
+
+terraform {
+  required_providers {
+    hcloud = {
+      source = "hetznercloud/hcloud"
+      version = ">=1.33.2"
+    }
+  }
+}
+
+provider "hcloud" {
+  token = var.hetzner_token
+}
+
+module "k3s" {
+  source          = "BerndDA/k3s/hcloud"
+  version         = "0.1.9"
+  # insert the 1 required variable here
+  hetzner_token = var.hetzner_token
+  cluster_name  = "k3s"
+  ssh_file        = "~/.ssh/id_ssh"
+  main_pool_config = {
+    node_type = "cx11"
+    num_nodes = 1
+  }
+  worker_pool_config = {
+    node_type = "cx21"
+    num_nodes = 2
+  }
+  k3s_version = "v1.24.3+k3s1"
+}
+
